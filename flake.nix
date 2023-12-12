1f967d4509ebceebc508ab1ce060ee0a0b86dbae
Moritz Angermann <moritz.angermann@gmail.com>
2023-12-12 06:43:00 +0000

    Try to fix db-sync.
    schema -> ../schema in the packages.

--- flake.nix
+++ flake.nix
@@ -386,22 +386,33 @@
           src = inputs.db-sync;
 
           inputMap = { "https://input-output-hk.github.io/cardano-haskell-packages" = inputs.CHaP; };
-          modules = [({
-            packages.double-conversion.ghcOptions = [
-              # stop putting U __gxx_personality_v0 into the library!
-              "-optcxx-fno-rtti" "-optcxx-fno-exceptions"
-              # stop putting U __cxa_guard_release into the library!
-              "-optcxx-std=gnu++98" "-optcxx-fno-threadsafe-statics"
-            ];
-          })
-          # Fix compilation with newer ghc versions
-          ({ lib, config, ... }:
-            lib.mkIf (lib.versionAtLeast config.compiler.version "9.4") {
-            # lib:ghc is a bit annoying in that it comes with it's own build-type:Custom, and then tries
-            # to call out to all kinds of silly tools that GHC doesn't really provide.
-            # For this reason, we try to get away without re-installing lib:ghc for now.
-            reinstallableLibGhc = false;
-          })
+          modules = [
+            {
+              # db-sync annoyingly has symlinks to the schema files, which are in ../schema,
+              # which in turn are stripped by nix away :-/
+              #
+              # TODO: Maybe we should just copy them over from ${src}/schema into schema in the postUnpack phase?
+              packages.cardano-db-sync.package.extraSrcFiles =
+                [ "../schema/*.sql" ];
+              packages.cardano-db.package.extraSrcFiles =
+                ["../config/pgpass-testnet"];
+            }
+            ({
+              packages.double-conversion.ghcOptions = [
+                # stop putting U __gxx_personality_v0 into the library!
+                "-optcxx-fno-rtti" "-optcxx-fno-exceptions"
+                # stop putting U __cxa_guard_release into the library!
+                "-optcxx-std=gnu++98" "-optcxx-fno-threadsafe-statics"
+              ];
+            })
+            # Fix compilation with newer ghc versions
+            ({ lib, config, ... }:
+              lib.mkIf (lib.versionAtLeast config.compiler.version "9.4") {
+              # lib:ghc is a bit annoying in that it comes with it's own build-type:Custom, and then tries
+              # to call out to all kinds of silly tools that GHC doesn't really provide.
+              # For this reason, we try to get away without re-installing lib:ghc for now.
+              reinstallableLibGhc = false;
+            })
           ];
         };
 
