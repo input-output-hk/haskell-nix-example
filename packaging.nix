e07b75969b7e277dfc022314d40b1500648ae474
Moritz Angermann <moritz.angermann@gmail.com>
2024-01-23 05:29:49 +0000

    Meh this zip behaviour again :facepalm:

--- packaging.nix
+++ packaging.nix
@@ -79,7 +79,7 @@ super: self: {
             # compress and put into hydra products
             installPhase = ''
                 mkdir -p $out/
-                zip -r -9 $out/${name'}.zip ${name'}/*
+                (cd ${name'} && zip -r -9 $out/${name'}.zip *)
 
                 mkdir -p $out/nix-support
                 echo "file binary-dist \"$(echo $out/*.zip)\"" \
