6875a87353d9b5ebe8e765f9475b313211b15ee0
Moritz Angermann <moritz.angermann@gmail.com>
2025-02-07 08:39:03 +0900

    Fixup libiconv.2.dylib

--- packaging.nix
+++ packaging.nix
@@ -20,6 +20,7 @@ super: self: {
             for nixlib in $(otool -L "$1" |awk '/nix\/store/{ print $1 }'); do
                 case "$nixlib" in
                 *libiconv.dylib)    install_name_tool -change "$nixlib" /usr/lib/libiconv.dylib "$1" ;;
+                *libiconv.*.dylib)  install_name_tool -change "$nixlib" /usr/lib/libiconv.dylib "$1" ;;
                 *libffi.*.dylib)    install_name_tool -change "$nixlib" /usr/lib/libffi.dylib   "$1" ;;
                 *libc++.*.dylib)    install_name_tool -change "$nixlib" /usr/lib/libc++.dylib   "$1" ;;
                 *libz.dylib)        install_name_tool -change "$nixlib" /usr/lib/libz.dylib     "$1" ;;
