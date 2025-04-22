699bdc73e048a1eef7621c2004d5509bc0cc142c
Moritz Angermann <moritz.angermann@gmail.com>
2025-04-22 14:45:00 +0900

    bump 10.3.1 pre-release

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     encoins.url = "github:encryptedcoins/encoins-relay";
     encoins.flake = false;
 
-    cardano-node.url = "github:IntersectMBO/cardano-node?ref=10.2.1";
+    cardano-node.url = "github:IntersectMBO/cardano-node?ref=10.3.1";
     cardano-node.flake = false;
 
     cardano-cli.url = "github:IntersectMBO/cardano-cli?ref=cardano-cli-10.3.0.0";
