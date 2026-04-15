be797ed95cbd58e5c887fe83d25667c3253b97c1
Moritz Angermann <moritz.angermann@gmail.com>
2026-04-15 15:13:36 +0200

    Update haskell.nix (fixes rsync permissions for VirtioFS)

--- flake.nix
+++ flake.nix
@@ -3,8 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    # Pin to the same haskell.nix that cardano-node 10.6.3 uses.
-    haskellNix.url = "github:input-output-hk/haskell.nix?rev=ef52c36b9835c77a255befe2a20075ba71e3bfab";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
     hackageNix = {
       url = "github:input-output-hk/hackage.nix";
       flake = false;
